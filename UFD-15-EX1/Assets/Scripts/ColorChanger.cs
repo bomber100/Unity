5312c46bb909d0885d97fe242324692e8f8af351
Aleksej Tolkachov <aleksej.tolkachov@gmail.com>
2018-03-02 22:49:06 +0200

    my first Unity task

---
+++ UFD-15-EX1/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+    float red = 0.225f;
+    float green = 0.54f;
+    float blue = 0.37f;
+    float alpha = 0.11f;
+    public float colorArgument = 0.01f;
+    Material m_Material;
+    // Use this for initialization
+    void Start () {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = new Color(red, green, blue, alpha);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ColorCorection();
+
+    }
+    void ColorCorection()
+    {
+        m_Material.color = new Color(red + colorArgument, green + colorArgument, blue + colorArgument, alpha);
+    }
+}
